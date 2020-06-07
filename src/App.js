88d3b3a6712e7c9a64069c529415b53f76f5f379
cakcay <cakcay@simpleviewinc.com>
2020-06-07 15:17:57 -0700

    Adding some basic components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const Header = () => (
+	<div className="header">
+		<h1>TODO LIST</h1>
+	</div>
+);
+
+const TodoList = ({ todo }) => <div className="todo-item">{todo.text}</div>
+
+const TodoForm = () => (
+	<form>
+		<input type="text" placeholder="Add new item..." />
+		<button>Add</button>
+	</form>
+);
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	const [todos, setTodos] = useState([
+		{ text: 'First thing to do today' },
+		{ text: 'Second thing to do today' },
+		{ text: 'Third thing to do today' },
+	]);
+
+	return (
+		<div className="app-container">
+			<Header />
+			<div className="todo-container">
+				{todos.map(todo => <TodoList todo={todo} />)}
+			</div>
+			<div className="add-todo-container">
+				<TodoForm />
+			</div>
+		</div>
+	);
 }
 
 export default App;
