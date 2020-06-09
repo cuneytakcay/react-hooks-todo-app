966a911152842ce960290edba8f33bbcb103ca24
cakcay <cakcay@simpleviewinc.com>
2020-06-08 17:47:12 -0700

    Updates

---
+++ precache-manifest.ad1858a08d32aef0dde42a038ff7565e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573c689e34ab2fa386061e5b04883946",
+    "url": "/react-hooks-todo-app/index.html"
+  },
+  {
+    "revision": "2550b8ad185457571aef",
+    "url": "/react-hooks-todo-app/static/css/main.f24b6859.chunk.css"
+  },
+  {
+    "revision": "f78685dd5650ad080eac",
+    "url": "/react-hooks-todo-app/static/js/2.587f5b08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-todo-app/static/js/2.587f5b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2550b8ad185457571aef",
+    "url": "/react-hooks-todo-app/static/js/main.859acad4.chunk.js"
+  },
+  {
+    "revision": "3ad9a7e7cac40f1f9ffe",
+    "url": "/react-hooks-todo-app/static/js/runtime-main.5dc56a8e.js"
+  }
+]);
